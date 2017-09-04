9e13acd5c9b1037990de5c221ce8b25b964b18d0
kmb4577 <kmb4577@gmail.rit.edu>
2017-09-04 17:30:31 -0600

    modeling-users
    - adjusted the users/registrations/edit form.
    - added permitted parameters for when a user goes to edit another user.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,7 +22,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     new_user_params
   end
 
-
   ##
   # POST /resource
   #
@@ -35,13 +34,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
     u_params = user_params
     @user = User.new(u_params.symbolize_keys)
     if @user.save
-      # current_user
+      # current_user will be present if a user is logged in
       # TODO: Capitalize first and last name before saving
       redirect_to users_path(), :flash => { :notice => "#{@user.first_name} #{@user.last_name} was #{I18n.t(:create_success_msg)}" }
     else
       #TODO:  display_message = obj_error_msgs(@user)
       redirect_to new_user_registration_path(), :flash => { :danger => "New user could not be registered. Please correct invalid form fields." }
     end
+  end
+
+
+  def update
+    params.permit(:first_name, :last_name, :role)
+    super
   end
 
   ##
