ba5b7756e09d077a549cd31df6867e9cb4175b3b
kmb4577 <kmb4577@gmail.rit.edu>
2017-09-05 17:31:00 -0600

    Adjusted some user role rights.

--- app/policies/access_policy.rb
+++ app/policies/access_policy.rb
@@ -8,8 +8,8 @@ class AccessPolicy
     # https://github.com/chaps-io/access-granted/blob/master/README.md
     #
     # Roles inherit from less important roles, so:
-    # - :admin has permissions defined in :member, :guest and himself
-    # - :member has permissions from :guest and himself
+    # - :admin has permissions defined in :employee, :guest and himself
+    # - :employee has permissions from :guest and himself
     # - :guest has only its own permissions since it's the first role.
     #
     # The most important role should be at the top.
@@ -18,27 +18,24 @@ class AccessPolicy
     role :admin, { role: "admin" } do
       can [:create, :update, :destroy, :change_role], User
     end
-    # role :admin, proc { |user| user.admin? } do
-    #   can [:update, :destroy], User
-    # end
 
+    ##
     # More privileged role, applies to registered users.
     #
     role :employee, proc { |user| user.employee? } do
-      # can :read, User
-
-      # can [:update, :destroy], Post do |post, user|
-      #   post.author = = user
-      # end
+      can :read, User
+      # May only edit their own user accounts
+      can [:update], User do |user_updating|
+        (user_updating == user)
+      end
     end
 
+    ##
     # The base role with no additional conditions.
     # Applies to every user.
     #
     role :guest do
       can :read, User
-    #  can :read, Post
-    #  can :read, Comment
     end
   end
 end
